72defb98c3a99fe577634275bc7b439184817e9d
Nathan Preel <nathan.preel@gmail.com>
2023-08-15 02:06:17 +0300

    feat: user sign up (#8)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Route } from '@angular/router';
+import { SignUpPageComponent } from './user';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'sign-up',
+    component: SignUpPageComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'sign-up',
+    pathMatch: 'full',
+  },
+];
---
+++ src/app/user/sign-up-page.component.ts
@@ -0,0 +1,38 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { APP_STORE } from '../app-store.token';
+import { Location } from '../location';
+import { SignUpFormComponent } from './sign-up-form.component';
+
+@Component({
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatCardModule,
+    SignUpFormComponent,
+  ],
+  template: `
+    <div class="mt-40 mx-80">
+      <mat-card>
+        <mat-card-header>
+          <mat-card-title>Sign Up</mat-card-title>
+        </mat-card-header>
+        <mat-card-content>
+          <app-sign-up-form (userSubmit)="setUserAndLocation($event)"></app-sign-up-form>
+        </mat-card-content>
+      </mat-card>
+    </div>
+  `,
+  styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SignUpPageComponent {
+  private readonly store = inject(APP_STORE);
+
+  setUserAndLocation(val: { firstName: string, lastName: string, location: Location}) {
+    const { firstName, lastName, location } = val;
+    this.store.setUser({ firstName, lastName });
+    this.store.setSelectedLocation(location);
+  }
+}
