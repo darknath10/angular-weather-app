f45f2364be9c480dccb777cb5cc8321e2d4782d2
Nathan Preel <nathan.preel@gmail.com>
2023-08-15 00:37:59 +0300

    feat: show current date time of the selected location

---
+++ src/app/clock/clock.component.ts
@@ -0,0 +1,39 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { interval, map } from 'rxjs';
+
+@Component({
+  selector: 'app-clock',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <div class="h-full flex items-center">
+      <span>{{now$ | async}}</span>
+    </div>
+  `,
+  styles: [
+    `
+      :host {
+        display: block;
+      }
+    `
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ClockComponent {
+  @Input() timezone = 'UTC';
+
+  now$ = interval(1000).pipe(
+    map(() =>
+      Intl.DateTimeFormat(undefined, {
+        timeZone: this.timezone,
+        weekday: 'short',
+        day: 'numeric',
+        month: 'short',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric'
+      }).format(Date.now())),
+  );
+}
